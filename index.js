1f7b84b59ef86d8befb0e4605bff793286b899f9
Akhil Goyal <akhilgoyal@akhils-mbp.lan>
2021-05-26 16:33:48 -0400

    Configured the job scheduler.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Connect to Twitter via API
+const express = require('express');
 const Twitter = require('twitter');
 const Sheet = require('./sheet');
+const cron = require('node-cron');
+
+const app = express();
 
 (async () => {
 
@@ -10,26 +14,40 @@ const Sheet = require('./sheet');
         access_token_key: '',
         access_token_secret: ''
     });
-    
+
     // Pull next quote from spreadsheet
     const sheet = new Sheet();
     await sheet.load();
 
     const quotes = await sheet.getRows();
-    console.log(quotes[0].quote);
+    const status = quotes[0].quote;
 
     // Send the tweet
-    // client.post('statuses/update', { status: 'I Love Twitter' }, function (error, tweet, response) {
-    
-    //     if (error) throw error;
-    
-    //     console.log(tweet);  // Tweet body.
-    
-    //     console.log(response);  // Raw response object.
-    
-    // });
-    
-    // Remove the quote from spreadsheet
+    client.post('statuses/update', { status }, (error, tweet, response) => {
+        if (error) throw error;
+        console.log(tweet);  // Tweet body.
+    });
 
-})()
+    // Remove the quote from spreadsheet
+    await quotes[0].delete();
+
+    console.log('Tweeted', quotes[0].quote);
+
+})();
+
+
+let testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+cron.schedule('* * * * * *', () => {
+    testArr.pop();
+    console.log('Popped!', testArr.length);
+    if (testArr.length === 0) {
+        // stop();
+        console.log('Empty');
+    }
+});
+
+app.listen(4000, () => {
+    console.log(`Server running...`);
+});
 
